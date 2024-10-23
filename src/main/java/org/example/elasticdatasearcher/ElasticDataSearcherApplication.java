b999620de2616a0252095733a3fba2fd98baecf0
Nina Alhimovich of 150502 <alhimovich.nina@gmail.com>
2024-10-23 21:40:22 +0300

    add database dependencies

---
+++ src/main/java/org/example/elasticdatasearcher/ElasticDataSearcherApplication.java
@@ -0,0 +1,11 @@
+package org.example.elasticdatasearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticDataSearcherApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticDataSearcherApplication.class, args);
+    }
+}
